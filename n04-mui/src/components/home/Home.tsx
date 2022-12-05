9223de179cd7bbd1356d7b90596e93a7bc34924e
trysd <itrysd@icloud.com>
2022-12-05 22:50:52 +0900

    n04 stylelint

--- n04-mui/src/components/home/Home.tsx
+++ n04-mui/src/components/home/Home.tsx
@@ -1,23 +1,27 @@
-import styles from './Home.module.scss';
+import { css } from "@emotion/react";
 import { v4 as uuid } from "uuid";
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 
 export const HomeComponent = () => {
+
   return (
-    <div className={styles.root}>
-      <p>Home: {uuid()}</p>
-      <Stack direction="row" spacing={2}>
-        <Button variant="outlined">login</Button>
-        {/* <LoadingButton
-          loading
-          loadingPosition="start"
-          startIcon={<SaveIcon />}
-          variant="outlined"
-        >
-          Save
-        </LoadingButton> */}
-      </Stack>
-    </div>
+    <section css={rootStyle}>
+      <div>
+        <p css={uuidStyle}>Home: {uuid()}</p>
+        <style>div</style>
+        <Stack direction="row" spacing={2}>
+          <Button variant="outlined">login</Button>
+        </Stack>
+      </div>
+    </section>
   );
 };
+
+const rootStyle = css`
+  background: #333;
+`;
+
+const uuidStyle = css`
+  color: #ed4134
+`;
--- n04-mui/vite.config.ts
+++ n04-mui/vite.config.ts
@@ -6,7 +6,9 @@ import svgr from 'vite-plugin-svgr'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
+    react({
+      jsxImportSource: '@emotion/react',
+    }),
     svgr(),
   ],
   base: './',
